6ed0a3d56f91c5a520b214e6c649eb942da13b38
Eduardo Salas Gonzalez <89312228+EduardoSalasG@users.noreply.github.com>
2025-12-09 18:00:08 -0300

    Fin sección 14

---
+++ 06-reactive-forms-app/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.routes'),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes'),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  }
+];
+
