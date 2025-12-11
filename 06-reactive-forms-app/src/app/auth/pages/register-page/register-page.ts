89b09a72b2a07ebe80dcc1c229268ca123c34765
Eduardo Salas Gonzalez <89312228+EduardoSalasG@users.noreply.github.com>
2025-12-11 15:59:34 -0300

    Fin sección 15

--- 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
+++ 06-reactive-forms-app/src/app/auth/pages/register-page/register-page.ts
@@ -1,9 +1,53 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule,],
   templateUrl: './register-page.html',
 })
-export class RegisterPage { }
+export class RegisterPage {
+
+  fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm = this.fb.group({
+    name: ['',
+      [
+        Validators.required,
+        Validators.pattern(this.formUtils.namePattern),
+      ]],
+    email: ['',
+      [
+        Validators.required,
+        Validators.pattern(this.formUtils.emailPattern),
+      ], [
+        this.formUtils.checkingServerResponse,
+      ]],
+    username: ['', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(this.formUtils.notOnlySpacesPattern),
+      this.formUtils.notStrider,
+    ]],
+    password: ['', [
+      Validators.required,
+      Validators.minLength(6),
+    ]],
+    confirmPassword: ['', Validators.required],
+  }, {
+    validators: [
+      this.formUtils.isFieldOneEqualFieldTwo('password', 'confirmPassword')
+    ]
+  })
+
+
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value)
+  }
+
+}
--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,8 +1,21 @@
-import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+
+async function sleep() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2500)
+  });
+}
 
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
 
   static getTextError(errors: ValidationErrors) {
+
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -13,7 +26,21 @@ export class FormUtils {
 
         case 'min':
           return `Valor mínimo de ${errors['min'].min}`
+        case 'email':
+          return `El valor ingresado no es un correo electrónico`
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El valor ingresado no luce como un correo electrónico'
+          }
+          return 'Error de patrón contra expresión regular'
+        case 'emailTaken':
+          return `El correo electrónico ya está siendo usuario por otro usuario`
+        case 'notStrider':
+          return `No se puede usar el username strider en la app`
 
+
+        default:
+          return `Error de validación no controlado ${key}`
       }
     }
     return null;
@@ -46,6 +73,41 @@ export class FormUtils {
     const errors = formArray.controls[index].errors ?? {};
 
     return FormUtils.getTextError(errors);
+
+  }
+
+  static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+
+    return (formGroup: AbstractControl) => {
+
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true }
+    }
+
+  }
+
+  static async checkingServerResponse(control: AbstractControl): Promise<ValidationErrors | null> {
+    console.log('Contra servidor')
+
+    await sleep();
+    const formValue = control.value;
+
+    if (formValue === 'hola@mundo.com') {
+      return {
+        emailTaken: true,
+      }
+    }
+
+    return null;
+
+  }
+
+  static notStrider(control: AbstractControl): ValidationErrors | null {
+    const formValue = control.value;
+
+    return formValue === 'strider' ? { notStrider: true } : null;
 
   }
 
